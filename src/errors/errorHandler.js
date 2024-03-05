26977f73fd3c1252deddd70da8706055f309e9af
jnmin567 <152448979+jnmin567@users.noreply.github.com>
2024-03-05 11:08:34 -0800

    Delete src directory

--- src/errors/errorHandler.js
+++
@@ -1,6 +0,0 @@
-function errorHandler(error, _req, res, _next) {
-  const { status = 500, message = 'Something went wrong!' } = error;
-  res.status(status).json({ error: message });
-}
-
-module.exports = errorHandler;
