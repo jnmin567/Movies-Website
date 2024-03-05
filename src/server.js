54de88e68bcbdc1d24bdd955b73a0a8739a98562
jnmin567 <152448979+jnmin567@users.noreply.github.com>
2024-03-04 21:09:32 -0800

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+
+const { PORT = 5000 } = process.env;
+
+const app = require('./app');
+const knex = require('./db/connection');
+
+app.use(cors());
+app.use(express.json()); 
+ 
+const listener = () => console.log(`Listening on Port ${PORT}!`);
+
+knex.migrate
+    .latest()
+    .then((migrations) => {
+        console.log('Migrations:', migrations);
+        app.listen(PORT, listener);
+    })
+    .catch(console.error);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
+});
